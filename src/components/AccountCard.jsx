c8d96bad6f00cb31ed812f0c39e653cfeb72dc2a
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-03-06 15:39:09 -0600

    feat: load account details

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -1,13 +1,13 @@
-import fnboLogo from '../assets/fnbo-logo.svg'
+import fnboLogo from '../assets/fnbo-logo.svg';
 
-export default function AccountCard() {
+export default function AccountCard({ name, number, bank }) {
   return (
-    <div className="card" style={{ width: '18rem' }}>
+    <div className="card" style={{ width: '10rem' }}>
       <img src={fnboLogo} className="card-img-top img-thumbnail" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Savings Account</h5>
-        <h6 className="card-subtitle mb-2 text-body-secondary">1234567890</h6>
-        <p className="card-text">First National Bank of Omaha</p>
+        <h5 className="card-title">{name}</h5>
+        <h6 className="card-subtitle mb-2 text-body-secondary">{number}</h6>
+        <p className="card-text">{bank}</p>
         <a href="#" className="btn btn-outline-secondary">
           Edit
         </a>
--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -1,11 +1,26 @@
+import { useState, useEffect } from 'react';
+import AccountCard from './AccountCard';
 
-import AccountCard from "./AccountCard";
+function getAccounts(setAccounts) {
+  fetch('http://localhost:3000/accounts')
+    .then((response) => response.json())
+    .then((data) => setAccounts(data.accounts));
+}
 
 export default function Accounts() {
-    return (
-        <>
-        <h2>Accounts Home</h2>
-        <AccountCard />
-        </>
-    );
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    getAccounts(setAccounts);
+  }, []);
+
+  return (
+    <div className="d-flex">
+      {accounts.map((item, index) => (
+        <div className="m-1">
+          <AccountCard key={index} {...item} />
+        </div>
+      ))}
+    </div>
+  );
 }
