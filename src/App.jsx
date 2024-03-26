ff4583df9a1da11d6cefac226dc1297b5e448a5a
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-03-26 16:21:54 -0500

    feat: update eslint

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,6 @@ function App() {
               <Route path="/accounts" element={<Accounts />} />
             </>
           ) : (
-            // Redirect to /login
             <Route path="*" element={<Navigate to="/login" />} />
           )}
         </Routes>
