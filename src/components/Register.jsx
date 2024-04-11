8280bc078e9136ae870fa7bf720eebb6bae64a4e
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-04-11 10:24:54 -0500

    feat: register form skeleton

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,4 @@
 import { useRef, useState, useEffect } from 'react';
-import { Form } from 'react-bootstrap';
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
@@ -12,42 +11,91 @@ const Register = () => {
   const [validName, setValidName] = useState(false);
   const [userFocus, setUserFocus] = useState(false);
 
+  const [pwd, setPwd] = useState('');
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState('');
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
   useEffect(() => {
     userRef.current.focus();
   }, []);
 
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPwd(result);
+    const match = pwd === matchPwd;
+    setValidMatch(match);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd, matchPwd]);
+
   return (
-    <div>
-      <h1>Register</h1>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Username"
-            id="username"
-            required
-            ref={userRef}
-            onChange={(e) => setUser(e.target.value)}
-            aria-describedby="username"
-            onFocus={() => setUserFocus(true)}
-            onBlur={() => setUserFocus(false)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" id="password" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Confirm Password"
-            id="confirmPassword"
-          />
-        </Form.Group>
-      </Form>
-    </div>
+    <section>
+      <div className="mb-4">
+        <h1>Register</h1>
+      </div>
+      <form>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="username">
+            Username
+            <input
+              className="form-control"
+              type="text"
+              name="username"
+              id="username"
+              placeholder="username"
+              required
+              autoComplete="off"
+              ref={userRef}
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
+            Password
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="confirmPassword">
+            Confirm Password
+            <input
+              className="form-control"
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="confirm password"
+            />
+          </label>
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Register
+        </button>
+      </form>
+    </section>
   );
 };
 
